62551221d1f0dcc60c0494a40dd9e90a3d25edc2
trakktor <107813840+Trakkt0r@users.noreply.github.com>
2024-12-13 00:46:52 +0100

    Solution to Day 11

--- day_11.py
+++ day_11.py
@@ -12,22 +12,22 @@ with open(__file__.rsplit("\\", 1)[0] + _file_name, "r") as file:
 
 
 @cache
-def apply_rules(rock, blink=0, blink_limit=25):
+def apply_rules(rock, blink_limit, blink=0):
     if blink == blink_limit:
         return 1
     if rock == 0:
-        return apply_rules(1, blink+1, blink_limit)
+        return apply_rules(1, blink_limit, blink+1)
     digits = int(log10(rock)) + 1
     if digits % 2 == 0:
         rock1, rock2 = divmod(rock, 10**(digits/2))
-        return apply_rules(rock1, blink+1, blink_limit) + apply_rules(rock2, blink+1, blink_limit)
-    return apply_rules(rock*2024, blink+1, blink_limit)
+        return apply_rules(rock1, blink_limit, blink+1) + apply_rules(rock2, blink_limit, blink+1)
+    return apply_rules(rock*2024, blink_limit, blink+1)
 
 
-def run(blink_limit=75):
+def run(blink_limit):
     answer = 0
     for rock in start_rocks:
-        answer += apply_rules(rock, blink_limit=blink_limit)
+        answer += apply_rules(rock, blink_limit)
 
     return answer
 
