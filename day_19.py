a922116eae8cd93132c0a9a2e45aa7134fced253
trakktor <107813840+Trakkt0r@users.noreply.github.com>
2024-12-24 04:01:15 +0100

    Solution to Day 19

--- day_19.py
+++ day_19.py
@@ -6,18 +6,26 @@ USE_TEST_INPUT = False
 _day = __file__.rsplit("\\", 1)[1][4:-3]
 _file_name = f"\\test_input_{_day}.txt" if USE_TEST_INPUT else f"\\input_{_day}.txt"
 
+with open(__file__.rsplit("\\", 1)[0] + _file_name, "r") as file:
+    input_patterns, designs = file.read().split("\n\n")
+    input_patterns = sorted(input_patterns.split(", "), key=len)
+    all_patterns = {length: tuple(group) for length, group in groupby(reversed(input_patterns), len)}
+    all_lengths = all_patterns.keys()
+    designs = designs.split("\n")[:-1]
 
-def is_possible(design, patterns, patterns_lengths):
+
+@cache
+def is_possible(design):
     design_length = len(design)
     if design_length == 0:
         return True
 
-    for pattern_length in patterns_lengths:
+    for pattern_length in all_lengths:
         if design_length < pattern_length:
             continue
 
-        if design[:pattern_length] in patterns[pattern_length]:
-            if not is_possible(design[pattern_length:], patterns, patterns_lengths):
+        if design[:pattern_length] in all_patterns[pattern_length]:
+            if not is_possible(design[pattern_length:]):
                 continue
             return True
 
@@ -41,33 +49,10 @@ def count_options(design):
     return answer
 
 
-def find_unique_patterns():
-    output = {}
-    output_lengths = []
-    for pattern in input_patterns:
-        pattern_length = len(pattern)
-
-        if not is_possible(pattern, output, output_lengths):
-            output.setdefault(pattern_length, []).append(pattern)
-            output_lengths.append(pattern_length)
-
-    return output, tuple(reversed(output_lengths))
-
-
-with open(__file__.rsplit("\\", 1)[0] + _file_name, "r") as file:
-    input_patterns, designs = file.read().split("\n\n")
-    input_patterns = sorted(input_patterns.split(", "), key=len)
-    all_patterns = {length: tuple(group) for length, group in groupby(reversed(input_patterns), len)}
-    all_lengths = all_patterns.keys()
-    unique_patterns, lengths = find_unique_patterns()
-    designs = designs.split("\n")[:-1]
-
-
 def part_one():
     answer = 0
     for design in designs:
-        if is_possible(design, all_patterns, lengths):
-            answer += 1
+        answer += is_possible(design)
 
     return answer
 
